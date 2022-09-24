a264d4214e87cd35a5a48de1633fdb8648511490
Caleb Foust <cfoust@air-caleb.mynetworksettings.com>
2022-09-24 13:10:12 -0400

    Basic working version

--- shrinkyrink_metro.py
+++ shrinkyrink_metro.py
@@ -1,10 +1,16 @@
-from datetime import datetime, date
-import json
-import requests
-import re
-import sys
 from bs4 import BeautifulSoup as soup
+from collections import namedtuple
+from datetime import datetime, date, timedelta
+import inquirer
+from inquirer.themes import GreenPassion
+import json
+import re
+import requests
+import sys
+
 from common import ShrinkyException
+
+Session = namedtuple('Session', ['id', 'time', 'data'])
 
 def validate(_time, _date=None):
     if not re.match("^\d\d:\d\d$", _time):
@@ -13,8 +19,7 @@ def validate(_time, _date=None):
     if _date and not re.match("^\d\d.\d\d.\d\d$", _date):
         raise ShrinkyException('Date is in invalid format')
 
-
-def sign_up(target_date, target_time, cookie):
+def log_in(cookie):
     session = requests.Session()
 
     home = session.get("https://opensports.net/my-games", headers={
@@ -40,45 +45,61 @@ def sign_up(target_date, target_time, cookie):
         "os-user-id": str(auth['os-user-id']),
     }
 
+    return (session, auth, auth_spread)
+
+
+def build_session(data):
+    time = datetime.strptime(data['start'][:16], "%Y-%m-%dT%H:%M")
+    return Session(data['id'], time, data)
+
+def get_sessions(handle):
+    session, auth, auth_spread = handle
+
     sessions = session.get("https://osapi.opensports.ca/app/posts/listFiltered", params={
-        'limit': 48,
+        'limit': 200,
         'groupIDs[]': 1905,
     }, headers=auth_spread).json()
 
     sessions = sessions['result']
-    time_parts = target_time.split(':')
-    target_datetime = datetime(
-        target_date.year,
-        target_date.month,
-        target_date.day,
-        int(time_parts[0]) + 4,
-        int(time_parts[1])
-    )
 
     freestyles = []
     for _session in sessions:
         if not 'Freestyle Session' in _session['title']: continue
-        start = datetime.strptime(_session['start'][:16], "%Y-%m-%dT%H:%M")
+        freestyles.append(build_session(_session))
 
-        if start != target_datetime: continue
+    return freestyles
 
-        freestyles.append(_session)
 
-    if not freestyles:
-        raise ShrinkyException('Failed to find freestyles for time.')
-
-    target = freestyles[0]
+def get_enrolled(handle):
+    session, auth, auth_spread = handle
 
     going = session.get(
         "https://osapi.opensports.ca/app/posts/listMyGoing",
         headers=auth_spread
     ).json()
+
+    freestyles = []
+    for _session in going['result']:
+        freestyles.append(build_session(_session))
+
+    return freestyles
+
+
+def sign_up(handle, target):
+    data = target.data
+    session, auth, auth_spread = handle
+
+    going = session.get(
+        "https://osapi.opensports.ca/app/posts/listMyGoing",
+        headers=auth_spread
+    ).json()
+
     existing = set(list(map(lambda v: v['id'], going['result'])))
 
-    if target['id'] in existing:
+    if target.id in existing:
         raise ShrinkyException('Already enrolled in session.')
 
-    summary = target['ticketsSummary'][0]
+    summary = data['ticketsSummary'][0]
     hold = session.post("https://osapi.opensports.ca/app/posts/insertHold",
         headers={
             "buildnumber": "202070",
@@ -106,7 +127,7 @@ def sign_up(target_date, target_time, cookie):
                     }
                 ],
                 "totalPriceInCents": summary['price'] * 100,
-                "postID": target['id']
+                "postID": target.id
             }
         )
     )
@@ -149,7 +170,7 @@ def sign_up(target_date, target_time, cookie):
                     }
                 ],
                 "totalPriceInCents": (summary['price'] * 100) / 4,
-                "postID": target['id'],
+                "postID": target.id,
                 "discountID": 801,
                 "numOfDiscounts": 1,
                 "membershipID": 3274
@@ -169,18 +190,90 @@ def sign_up(target_date, target_time, cookie):
 if __name__ == '__main__':
     args = sys.argv[1:]
 
-    if len(args) < 3:
-        print('You must provide your username, password, and desired time.')
+    if len(args) != 1:
+        print('You must provide a cookie.')
         exit(1)
 
-    target_date = date.today()
     cookie = args[0]
-    target_time = args[1]
 
-    if len(args) == 3:
-        cookie = args[0]
-        target_date = datetime.strptime(args[1], "%d.%m.%y")
-        target_date = date(target_date.year, target_date.month, target_date.day)
-        target_time = args[2]
+    handle = log_in(cookie)
 
-    sign_up(target_date, target_time, cookie)
+    today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+
+    days = list(map(lambda i: today + timedelta(days = i), list(range(0, 7))))
+
+    sessions = get_sessions(handle)
+
+    while True:
+        enrolled_sessions = get_enrolled(handle)
+        enrolled_set = set(list(map(lambda v: v.id, enrolled_sessions)))
+
+        day_choices = []
+        for day in days:
+            day_end = day + timedelta(days = 1)
+
+            day_sessions = list(filter(lambda v: v.time > day and v.time < day_end, sessions))
+            day_sessions = list(map(lambda v: (v, v.id in enrolled_set), day_sessions))
+            day_sessions = sorted(day_sessions, key=lambda v: v[0].time)
+
+            data = (day, day_sessions)
+            day_title = "{} ({}/{})".format(
+                day.strftime("%m/%d/%y %A"),
+                len(list(filter(lambda v: v[1], day_sessions))),
+                len(day_sessions)
+            )
+
+            day_choices.append(
+                (
+                    day_title,
+                    data
+                )
+            )
+
+        day, day_sessions = inquirer.list_input(
+            "Choose a day",
+            choices=day_choices
+        )
+
+        # Find all training sessions in the day, defined as contiguous periods
+        # of 1.5h or 2h
+        training_choices = []
+
+        for start, session_pair in enumerate(day_sessions):
+            session, enrolled = session_pair
+
+            for end in range(start + 2, start + 4):
+                training_sessions = day_sessions[start:end]
+
+                if len(training_sessions) != (end - start): continue
+
+                # Training sessions need to be contiguous
+                is_contiguous = True
+                for first, second in zip(training_sessions, training_sessions[1:]):
+                    if (
+                            second[0].time - (first[0].time + timedelta(minutes = 30))
+                    ) > timedelta(minutes = 10):
+                        is_contiguous = False
+                        break
+
+                if not is_contiguous: continue
+
+                # Format title
+                training_title = "{}-{} ({}) {}".format(
+                    training_sessions[0][0].time.strftime("%I:%M%p"),
+                    (training_sessions[-1][0].time + timedelta(minutes = 30)).strftime("%I:%M%p"),
+                    len(training_sessions),
+                    "".join(list(map(lambda v: '|' if v[1] else '.', training_sessions)))
+                )
+                training_choices.append(
+                    (training_title, training_sessions)
+                )
+
+        training_sessions = inquirer.list_input(
+            "Choose a training session",
+            choices=training_choices
+        )
+
+        for session, enrolled in training_sessions:
+            if enrolled: continue
+            sign_up(handle, session)
